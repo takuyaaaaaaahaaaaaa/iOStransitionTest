bfb0d3c2e335633e1ee202338e3c24fd9697cf07
takuyaaaaaaahaaaaaa <majikani2011@gmail.com>
2022-08-07 14:33:32 +0900

    Initial Commit

---
+++ iPadTransitionTest/iPadTransitionTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPadTransitionTestApp.swift
+//  iPadTransitionTest
+//
+//  Created by takuya_tominaga on 8/7/22.
+//
+
+import SwiftUI
+
+@main
+struct iPadTransitionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
